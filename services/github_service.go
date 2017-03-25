cda893063ff322dd069776fb4294e1938b6b6288
Tom Bell <tomb@tomb.io>
2017-03-25 01:53:45 +0000

    Add github_service.go and document process

---
+++ services/github_service.go
@@ -0,0 +1,14 @@
+package services
+
+// parse incoming webhook type
+
+// - issue/pull request opened
+// - issue/pull request comment created
+
+// parse the webhook body depending on type
+
+// does the user have contributer or high access to the repo?
+
+// does the body contain the specified string?
+
+// add the label to the issue/pull request with the specified label
