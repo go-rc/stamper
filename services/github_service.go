d8ba752ca6eb15575abee46ce0093d6941a6e7aa
Tom Bell <tomb@tomb.io>
2017-03-25 14:56:43 +0000

    Fix typo in comment

--- services/github_service.go
+++ services/github_service.go
@@ -7,7 +7,7 @@ package services
 
 // parse the webhook body depending on type
 
-// does the user have contributer or high access to the repo?
+// does the user have contributer or higher access to the repo?
 // https://developer.github.com/v3/repos/collaborators/#review-a-users-permission-level
 
 // does the body contain the specified string?
