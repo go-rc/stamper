f0bb5e21880b56dbddf2e2f6514e5e05e3a9a712
Tom Bell <tomb@tomb.io>
2017-03-25 19:54:54 +0000

    Remove comments

--- services/github_service.go
+++ services/github_service.go
@@ -7,21 +7,6 @@ import (
 	"github.com/tombell/stamper/services/clients"
 )
 
-// parse incoming webhook type
-
-// - issue/pull request opened
-// - issue/pull request comment created
-
-// parse the webhook body depending on type
-
-// does the user have contributer or higher access to the repo?
-// https://developer.github.com/v3/repos/collaborators/#review-a-users-permission-level
-
-// does the body contain the specified string?
-
-// add the label to the issue/pull request with the specified label
-// https://developer.github.com/v3/issues/#edit-an-issue
-
 type OpenedEvent struct {
 	Action      string     `json:"action"`
 	Repository  Repository `json:"repository"`
