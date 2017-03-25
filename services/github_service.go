2c71a97bfed09c0bf7be2d2bbe1066e658b55c34
Tom Bell <tomb@tomb.io>
2017-03-25 13:35:30 +0000

    Add links to the API docs for API requests needed

--- services/github_service.go
+++ services/github_service.go
@@ -8,7 +8,9 @@ package services
 // parse the webhook body depending on type
 
 // does the user have contributer or high access to the repo?
+// https://developer.github.com/v3/repos/collaborators/#review-a-users-permission-level
 
 // does the body contain the specified string?
 
 // add the label to the issue/pull request with the specified label
+// https://developer.github.com/v3/issues/#edit-an-issue
