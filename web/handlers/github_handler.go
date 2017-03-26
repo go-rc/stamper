c2e387efd64150dea599f495ff8b61829c658f32
Tom Bell <tomb@tomb.io>
2017-03-26 18:23:22 +0100

    Update comments in github handler

--- web/handlers/github_handler.go
+++ web/handlers/github_handler.go
@@ -9,11 +9,11 @@ import (
 	"github.com/tombell/stamper/web/middleware"
 )
 
-// GitHubHandler is the http.Handler for handling incoming GitHub webook
-// requests.
+// GitHubHandler is the http.Handler for incoming GitHub webhook requests.
 func GitHubHandler(w http.ResponseWriter, r *http.Request) {
 	srv, ok := r.Context().Value(middleware.GitHubServiceContextKey).(*services.GitHubService)
 	if !ok {
+		// TODO
 		panic("could not get github service from request context")
 	}
 
